c8d57390561b945b3eaf0c8cfab6a3f9e45c1848
Nura Renke <nurarenke@gmail.com>
2017-06-10 17:20:57 +0000

    inital commit

---
+++ server.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+from jinja2 import StrictUndefined
+
+
+app = Flask(__name__)
+app.jinja_env.undefined = StrictUndefined
+app.jinja_env.auto_reload = True
+
+# Required to use Flask sessions and the debug toolbar
+app.secret_key = "ABC"
+
+
+# YOUR ROUTES GO HERE
+@app.route('/')
+def view_homepage():
+    '''Display homepage.'''
+
+    return render_template("index.html")
+
+@app.route('/application-form')
+def view_application_form():
+    '''Display application-form for job application.'''
+
+    return render_template("application-form.html")
+
+
+if __name__ == "__main__":
+    # We have to set debug=True here, since it has to be True at the
+    # point that we invoke the DebugToolbarExtension
+    app.debug = True
+
+    # Use the DebugToolbar
+    DebugToolbarExtension(app)
+
+    app.run(host="0.0.0.0")
